162f0fe3a6396bc7daf824ebf80957fcc15f00e1
unknown <karthikeyan1296@yahoo.com>
2020-07-23 12:38:51 +0530

    sending data through url

--- p5/urls.py
+++ p5/urls.py
@@ -23,5 +23,11 @@ urlpatterns = [
     path('first/',views.first,name="first"),
     path('second/',views.second,name="second"),
     path('third/',views.third,name="third"),
-    path('fourth/',views.fourth,name="fourth")
+    path('fourth/',views.fourth,name="fourth"),
+    path('fifth/',views.fifth,name="fifth"),
+    path('urls_data/<name>',views.urls_data,name="urls_data"),
+    path('urls_data1/<ab>',views.urls_data1,name="urls_data1"),
+    path('urls_data2/<c>/<d>',views.urls_data2,name="urls_data2"),
+    path('urls_data3/<greater>',views.urls_data3,name="urls_data3"),
+    path('urls_data4/<string>',views.urls_data4,name="urls_data4")
 ]
--- p5/views.py
+++ p5/views.py
@@ -16,3 +16,37 @@ def third(request):
 def fourth(request):
     fruits=['apple','orange','kiwi','watermelon','musk melon']
     return render(request,"directory/fourth.html",{'fruits':fruits})
+
+def fifth(request):
+    return render(request,"directory/fifth.html",context={'a':"10",'b':"20"})
+
+def urls_data(request,name):
+    return HttpResponse("<h1>{}<h1>".format(name))
+
+def urls_data1(request,ab):   #adding two no. by using single argument(ab)
+    a=ab.split(" ")
+    sum=int(a[0])+int(a[1])
+    return HttpResponse(str(sum))
+
+def urls_data2(request,c,d):   #adding two no. by using two argument(c,d)
+    sum=int(c)+int(d)
+    return HttpResponse(str(sum))
+
+def urls_data3(request,greater):
+    g=greater.split(" ")
+    if int(g[0]) > int(g[1]):
+        return HttpResponse(str(g[0]))
+
+    else:
+        return HttpResponse(str(g[1]))
+
+def urls_data4(request,string):
+    vowels = 'aeiouAEIOU'
+    vow = 0
+    con = 0
+    for i in string:
+        if i in vowels:
+            vow+=1
+        else:
+            con+=1
+    return render(request,"directory/vowel.html",context={'v':vow,'c':con})
